39ca524cc3e66f86d0ade87cc89fb35abe23d96b
Ankita Sharma <shraddhaasharma1507@gmail.com>
2026-05-28 20:49:13 +0530

    This is the new solution of the problem
    In this problem, I used the optimized approach which is using the 3 variable and then using the reminder of the number adding it to the reverse , and them again dividing the same variable with 10.
    And then again comparing to the temporary variable

--- leetcodeproblem9.java
+++ leetcodeproblem9.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-       String s = Integer.toString(x);
-        int i =0; int j = s.length()-1;
-        while (i<j){
-            if (s.charAt(i)== s.charAt(j)){
-                return true;
-            }
-            i++; j--;
+        if(x<0){
+            return false;
         }
-       return false;
+        int rev=0; int temp=x;
+        while (temp>0){
+            rev= rev*10+(temp%10);
+            temp = temp/10;
+        }
+        if (rev==x){
+            return true;
+        }
+        return false;
     }
 }
