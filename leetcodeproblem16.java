85abc23511520da4fe20bea552b644335f3584ba
Ankita Sharma <shraddhaasharma1507@gmail.com>
2026-06-28 22:38:49 +0530

    solution of leetcode probelm 16

---
+++ leetcodeproblem16.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int closest = nums[0]+nums[1]+nums[2];
+        for (int i =0;i< nums.length; i++){
+            for (int j=i+1; j<nums.length;j++){
+                for(int k = j+1; k<nums.length;k++){
+                    int sum =nums[i]+nums[j]+nums[k];
+                    if (sum == target){
+                        return sum;
+                    }
+                    if (Math.abs(sum-target) < Math.abs(closest-target)){
+                        closest = sum;
+                    }
+                    }
+                }
+            }
+        
+        return closest;
+    }
+}
